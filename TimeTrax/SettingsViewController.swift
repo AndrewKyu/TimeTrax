d1a7c28527f2436ad5e9984fd91289e7e0713634
AndrewKyu <andrewkyu95@gmail.com>
2018-04-23 20:01:49 -0700

    able to log out through settings tab

---
+++ TimeTrax/SettingsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SettingsViewController.swift
+//  TimeTrax
+//
+//  Created by Andrew Kyu on 4/23/18.
+//  Copyright © 2018 AndrewKyuProjects. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SettingsViewController: UIViewController {
+
+    
+    @IBAction func logoutButtonPressed(_ sender: Any) {
+        do{
+            try Auth.auth().signOut()
+            performSegue(withIdentifier: "goToLogin", sender: self)
+        }catch let error as NSError{
+            print(error.localizedDescription)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
