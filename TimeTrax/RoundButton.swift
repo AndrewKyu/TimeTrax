917cf902f561938eb61c4ec68f54e471d872e172
AndrewKyu <andrewkyu95@gmail.com>
2018-04-27 19:29:55 -0700

    Created RoundedButton class, created segue on click of the + button to go to CreateProjects View

---
+++ TimeTrax/RoundButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundButton.swift
+//  TimeTrax
+//
+//  Created by Andrew Kyu on 4/27/18.
+//  Copyright © 2018 AndrewKyuProjects. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
