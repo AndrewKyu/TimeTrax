18733445717f65ee76e4882acbf8acfc5d0d9d33
AndrewKyu <andrewkyu95@gmail.com>
2018-04-29 23:37:18 -0700

    Fixed task label update, members view

--- TimeTrax/TaskDetailViewController.swift
+++ TimeTrax/TaskDetailViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import FirebaseAuthUI
 import FirebaseGoogleAuthUI
+import FirebaseDatabase
+
 
 
 class TaskDetailViewController: UIViewController {
@@ -24,6 +26,50 @@ class TaskDetailViewController: UIViewController {
     
     var task:taskType?
     
+//    var tasks = [TaskModel]()
+//
+//
+//
+//    fileprivate func fetchData(){
+//        let reference = Database.database().reference().child("task")
+//        reference.observeSingleEvent(of: DataEventType.value) { (snapshot) in
+//            //print(snapshot.value)
+//            guard let tasksDictionary = snapshot.value as? [String: Any] else{ return }
+//            print(tasksDictionary)
+//
+//            //clears out array
+//            self.tasks = []
+//
+//            tasksDictionary.forEach({ (key, value) in
+//                if let individualTasks = value as? [String: String]{
+//                    print(individualTasks)
+//                    guard let taskName = individualTasks["taskName"] else{
+//                        return
+//                    }
+//                    guard let priorityNumber = individualTasks["priority"] else{
+//                        return
+//                    }
+//                    guard let dueDate = individualTasks["dueDate"] else{
+//                        return
+//                    }
+//                    guard let startDate = individualTasks["startDate"] else{
+//                        return
+//                    }
+//                    guard let projectName = individualTasks["projectName"] else{
+//                        return
+//                    }
+//                    print(taskName, priorityNumber, dueDate, startDate)
+//                    let task = TaskModel(taskName: taskName, startDate: startDate, dueDate: dueDate, priority: priorityNumber, projectName: projectName)
+//
+//                    self.tasks.append(task)
+//                }
+//
+//            })
+//            DispatchQueue.main.async {
+//                self.taskViewControllr.reloadData()
+//            }
+//        }
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
