e501a3da0c7ba4df883946267888156f46542e3e
ColePBryan <bryan.cole@sjsu.edu>
2018-04-29 17:31:03 -0700

    added user and detail project

---
+++ TimeTrax/ProjectDetailsViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ProjectDetailsViewController.swift
+//  TimeTrax
+//
+//  Created by Bryan Cole on 4/28/18.
+//  Copyright © 2018 AndrewKyuProjects. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+import Firebase
+import FirebaseAuthUI
+
+class ProjectDetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+    
+    var project: ProjectModel?
+    
+    
+
+    @IBOutlet weak var NavBar: UINavigationBar!
+    @IBOutlet weak var ProjectDescription: UITextView!
+    @IBOutlet weak var startDate: UILabel!
+    @IBOutlet weak var EndDate: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    static func fromStoryboard(_ storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)) -> ProjectDetailsViewController {
+        let controller = storyboard.instantiateViewController(withIdentifier: "ProjectDetailsViewController") as! ProjectDetailsViewController
+        return controller
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 20
+        
+    
+    }
+    
+    deinit {
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+     */
+    
+
+
+}
+
+class TeamMemberViewCell: UITableViewCell {
+//    @IBOutlet var UserName: UILabel!
+    
+    func populate(user: User){
+//        UserName.text = user.firstName + " " + user.lastName
+    }
+    
+    
+}
---
+++ TimeTrax/User.swift
@@ -0,0 +1,29 @@
+//
+//  User.swift
+//  TimeTrax
+//
+//  Created by Bryan Cole on 4/28/18.
+//  Copyright © 2018 AndrewKyuProjects. All rights reserved.
+//
+
+import Foundation
+
+struct User{
+    var email: String
+    var firstName: String
+    var lastName: String
+    var phoneNumber: String
+    var jobTitle: String //could be enum
+    var admin: Bool
+    
+    var dictionary: [String: Any] {
+        return [
+            "lastName" : lastName,
+            "firstName" : firstName,
+            "email" : email,
+            "phoneNumber" : phoneNumber,
+            "jobTitle" : jobTitle,
+            "admin" : admin,
+        ]
+    }
+}
