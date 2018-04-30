25bfb4a02426821c5375bfd583a45f4be454b72e
ColePBryan <bryan.cole@sjsu.edu>
2018-04-29 17:26:57 -0700

    successx

--- TimeTrax/ProjectModel.swift
+++ TimeTrax/ProjectModel.swift
@@ -14,3 +14,23 @@ struct ProjectModel{
     let groupMember: String?
     
 }
+
+//struct Project{
+//    var title: String
+//    var description: String
+//    var usersAssigned: [User]
+//    var startDate: Date
+//    var dueDate: Date
+//    var endDate: Date
+//    
+//    var dictionary: [String: Any]{
+//        return[
+//            "title" : title,
+//            "description" : description,
+//            "usersAssigned" : usersAssigned,
+//            "startDate" : startDate,
+//            "dueDate" : dueDate,
+//            "endDate" : endDate,
+//        ]
+//    }
+//}
